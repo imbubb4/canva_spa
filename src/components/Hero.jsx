d4a7b6d9b34cea95c6d3effed937fc93125f1693
Adrian <teebee15@hotmail.com>
2025-07-02 14:17:00 -0500

    asd

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 const Hero = () => {
     return (
         <div className="px-4 py-5 my-5 text-center"> <img className="d-block mx-auto mb-4"
-            src="/canva.png" alt="canva" width="72" height="57"/>
+            src={`${import.meta.env.BASE_URL}canva.png`} alt="canva" width="72" height="57"/>
             <h1 className="display-5 fw-bold text-body-emphasis">Centered hero</h1>
             <div className="col-lg-6 mx-auto">
                 <p className="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most
